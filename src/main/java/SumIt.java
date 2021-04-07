79f70e50791513609e63ec33de8ca53abc6aa685
jan.sedlak <jan.sedlak@slovenskoit.sk>
2021-04-07 11:37:17 +0200

    Floating point arithmetic.

---
+++ src/main/java/SumIt.java
@@ -0,0 +1,7 @@
+public class SumIt {
+
+    public static void main(String[] args) {
+        double doubleValue = Double.sum(0.1d, 0.2d);
+        System.out.println("Double sum:" + doubleValue);
+    }
+}
